44785d7cb04ff614809f6a0e1f6e2e798190edf0
alwaysfish <ed091maf@gmail.com>
2020-10-08 00:50:43 +0100

    Updated optimization functions

--- alphalib/efficient_frontier.py
+++ alphalib/efficient_frontier.py
@@ -23,17 +23,25 @@ class EfficientFrontier:
         self.n_assets = len(er)
         self.asset_names = er.index.values
 
-    def min_variance(self):
+    def min_volatility(self, *, target_ret=None):
         """
-        Returns weights of the portfolio with minimum variance.
+        Returns weights of the portfolio with minimum volatility.
+
+        Arguments:
+            target_ret: float, default=None
+                Target return, for which to find a portfolio with lowest volatility. If target_ret is None,
+                then weights for Global Minimum-Variance portfolio are returned.
 
         Returns:
             weights: pd.Series
         """
 
-        return self._optimize(0, gmv=True)
+        if target_ret is None:
+            return self._optimize(0, gmv=True)
+        else:
+            return self._optimize_for_target('min_vol', target_ret)
 
-    def max_sharpe(self, rf):
+    def max_sharpe(self, *, rf):
         """
         Returns weights of the portfolio with the highest Sharpe ratio.
 
@@ -47,51 +55,24 @@ class EfficientFrontier:
         """
         return self._optimize(rf)
 
-    def _optimize(self, rf, gmv=False):
+    def max_return(self, *, target_vol):
         """
-        Finds weights of a optimal portfolio. If gmv (Global Minimum-Variance portfolio) 
-        is False, then a portfolio with highest Sharpe ratio is found. Otherwise, a global
-        minimum-variance portfolio is found.
+        Returns weights of the portfolio with the highest return for a given traget volatility.
 
         Arguments:
-            rf: float
-                Risk free rate. The period of the risk free rate must correspond to the
-                frequency of expected returns.
-            
-            gmv: bool
-                True, if global minimum-variance portfolio should be found.
-                False, if a portfolio with highest Sharpe ratio should be found.
+            target_vol: float
+                Target volatility for which to find a portfolio with the highest return.
 
         Returns:
-            pd.Series
-                Weights of the optimal portfolio.
+            weights: pd.Series
         """
+        return self._optimize_for_target('max_ret', target_vol)
 
-         # set bounds for weights
-        w_bounds = ((0, 1),) * self.n_assets
-
-        # set initial weights
-        w_init = np.repeat(1 / self.n_assets, self.n_assets)
-
-        # constraint 1: weights add to 1
-        cons_weights_sum1 = {
-            'type': 'eq',
-            'fun': lambda weights: np.sum(weights) - 1
-        }
-
-        if gmv:
-            er = np.repeat(0.01, self.n_assets)
-        else:
-            er = self.er
-
-        def neg_sharpe(w, r, cov_mat, rf):
-            return -Portfolio.get_sharpe(w, r, cov_mat, rf)
-        
-        results = minimize(neg_sharpe, w_init, args=(er, self.cov_mat, rf), method='SLSQP',
-            options={'disp': False}, constraints=(cons_weights_sum1),
-            bounds=w_bounds)
-
-        return pd.Series(results.x, index=self.asset_names)
+    def plot(self):
+        """
+        Plots Efficient Frontier curve.
+        """
+        raise NotImplemented()
 
     def get_random_portfolios(self, n=100, return_weights=False):
         """
@@ -111,7 +92,7 @@ class EfficientFrontier:
 
         weights = self._get_rand_weights(n)
 
-        p_r = Portfolio.get_returns(weights, self.er)
+        p_r = Portfolio.get_return(weights, self.er)
         p_sigma = Portfolio.get_volatility(weights, self.cov_mat)
 
         p_df = pd.DataFrame({'return': p_r, 'volatility': p_sigma})
@@ -138,31 +119,90 @@ class EfficientFrontier:
         w = np.random.rand(n, self.n_assets)
         return pd.DataFrame(w / np.sum(w, axis=1, keepdims=True), columns=self.asset_names)
 
-    def _minimize_volatility(self, target_ret):
-        """
-        """
-
-        # set bounds for weights
-        w_bounds = ((0, 1),) * self.n_assets
+    def _init_optimization(self):
 
         # set initial weights
         w_init = np.repeat(1 / self.n_assets, self.n_assets)
 
+        # set bounds for weights
+        w_bounds = ((0, 1),) * self.n_assets
+
         # constraint 1: weights add to 1
-        cons_weights_sum1 = {
+        constraint = {
             'type': 'eq',
             'fun': lambda weights: np.sum(weights) - 1
         }
-        
-        # constraint 2: portfolio return match the target return
-        cons_return_eq_target = {
-            'type': 'eq',
-            'args': (self.er, ),
-            'fun': lambda weights, er: Portfolio.get_returns(weights, er) - target_ret
-        }
-        
-        results = minimize(Portfolio.get_volatility, w_init, args=(self.cov_mat,), method='SLSQP',
-            options={'disp': False}, constraints=(cons_weights_sum1, cons_return_eq_target),
-            bounds=w_bounds)
 
-        return pd.Series(results.x, index=self.asset_names), results.fun, results.success
+        return w_init, w_bounds, constraint
+
+    def _optimize(self, rf, gmv=False):
+        """
+        Finds weights of a optimal portfolio. If gmv (Global Minimum-Variance portfolio)
+        is False, then a portfolio with highest Sharpe ratio is found. Otherwise, a global
+        minimum-variance portfolio is found.
+
+        Arguments:
+            rf: float
+                Risk free rate. The period of the risk free rate must correspond to the
+                frequency of expected returns.
+
+            gmv: bool
+                True, if global minimum-variance portfolio should be found.
+                False, if a portfolio with highest Sharpe ratio should be found.
+
+        Returns:
+            pd.Series
+                Weights of the optimal portfolio.
+        """
+
+        w_init, w_bounds, constr1 = self._init_optimization()
+
+        if gmv:
+            er = np.repeat(0.01, self.n_assets)
+        else:
+            er = self.er
+
+        def neg_sharpe(w, r, cov_mat, _rf):
+            return -Portfolio.get_sharpe(w, r, cov_mat, _rf)
+
+        results = minimize(neg_sharpe, w_init, args=(er, self.cov_mat, rf), method='SLSQP',
+                           options={'disp': False}, constraints=constr1,
+                           bounds=w_bounds)
+
+        return pd.Series(results.x, index=self.asset_names)
+
+    def _optimize_for_target(self, optimization, target):
+
+        w_init, w_bounds, constr1 = self._init_optimization()
+
+        if optimization == 'max_ret':
+            constr2 = {
+                'type': 'eq',
+                'args': (self.cov_mat,),
+                'fun': lambda weights, cov_mat: Portfolio.get_volatility(weights, cov_mat) - target
+            }
+            args = (self.er,)
+
+            def neg_portf_return(w, er):
+                return -Portfolio.get_return(w, er)
+
+            func = neg_portf_return
+
+        elif optimization == 'min_vol':
+            constr2 = {
+                'type': 'eq',
+                'args': (self.er,),
+                'fun': lambda weights, er: Portfolio.get_return(weights, er) - target
+            }
+            args = (self.cov_mat,)
+            func = Portfolio.get_volatility
+        else:
+            raise ValueError('Only max_ret and min_vol optimizations are accepted')
+
+        results = minimize(func, w_init,
+                           args=args, method='SLSQP',
+                           options={'disp': False},
+                           constraints=[constr1, constr2],
+                           bounds=w_bounds)
+
+        return pd.Series(results.x, index=self.asset_names)
