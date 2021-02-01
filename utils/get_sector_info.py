1e50c5cbe30ba333f0a47364c7eefe4e7a42a88f
alwaysfish <ed091maf@gmail.com>
2021-02-01 12:38:06 +0000

    Historical prices are stored to PostgreSQL

--- utils/get_sector_info.py
+++ utils/get_sector_info.py
@@ -8,6 +8,6 @@ if df is None:
     exit(1)
 
 print('Saving data into CSV')
-df.to_csv('./data/baltic_equities_info.csv', index=False, float_format='%.6g')
+df.to_csv('../data/baltic_equities_info.csv', index=False, float_format='%.6g')
 
 print('Done!')
